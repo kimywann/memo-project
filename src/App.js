7a8a3d4b01b7ed6d0b5a9cb801b81c7f5f0bdaed
kimywann <Kimywan10@gmail.com>
2025-01-06 21:38:31 +0900

    Accordion 메뉴바 컴포넌트 예제 연습

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import Accordion from './components/Accordion';
 
 function App() {
-  return (
-    <div
-      style={{ fontSize: '2rem', padding: 30 }}
-    >
-      <Accordion
-        title= "This is a Title"
-      >
-
-      </Accordion>
-
-    </div>
-  );
+    return (
+        <div style={{ fontSize: '2rem', padding: 30 }}>
+            <Accordion title="This is a Title" content="This is a content" />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,28 +1,39 @@
-// function Accordion({ title, content }) {
-//   return (
-//     <div>
-//       <div
-//         style={{
-//           background: '#666',
-//           color: 'white',
-//           fontWeight: 'bold',
-//           padding: 10,
-//           display: 'flex',
-//           justifyContent: 'space-between',
-//         }}
-//       >
-//         타이틀
-//       </div>
-//       <div
-//         style={{
-//           border: '1px solid #999',
-//           padding: 20,
-//         }}
-//       >
-//         콘텐츠
-//       </div>
-//     </div>
-//   );
-// }
+import App from '../App';
+import { useState } from 'react';
 
-// export default Accordion;
+function Accordion({ title, content }) {
+    const [isOpened, setIsOpened] = useState(false);
+
+    return (
+        <div>
+            <div
+                style={{
+                    background: '#666',
+                    color: 'white',
+                    fontWeight: 'bold',
+                    padding: 10,
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                }}
+                onClick={() => {
+                    setIsOpened((state) => !state);
+                }}
+            >
+                <div>{title}</div>
+                <div>{isOpened ? '-' : '+'}</div>
+            </div>
+            {isOpened && (
+                <div
+                    style={{
+                        border: '1px solid #999',
+                        padding: 20,
+                    }}
+                >
+                    {content}
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Accordion;
