87262479058271aaaf5554013ecfc602544414db
kimywann <Kimywan10@gmail.com>
2025-01-06 20:44:30 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Accordion from './components/Accordion';
+
+function App() {
+  return (
+    <div
+      style={{ fontSize: '2rem', padding: 30 }}
+    >
+      <Accordion
+        title= "This is a Title"
+      >
+
+      </Accordion>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Accordion.js
@@ -0,0 +1,28 @@
+// function Accordion({ title, content }) {
+//   return (
+//     <div>
+//       <div
+//         style={{
+//           background: '#666',
+//           color: 'white',
+//           fontWeight: 'bold',
+//           padding: 10,
+//           display: 'flex',
+//           justifyContent: 'space-between',
+//         }}
+//       >
+//         타이틀
+//       </div>
+//       <div
+//         style={{
+//           border: '1px solid #999',
+//           padding: 20,
+//         }}
+//       >
+//         콘텐츠
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Accordion;
