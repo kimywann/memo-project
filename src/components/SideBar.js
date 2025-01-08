9d96ba87c7d923b0eb32938034fd45a31b9c0b03
kimywann <Kimywan10@gmail.com>
2025-01-08 22:03:29 +0900

    refactor: SideBar에서 props 전달 구조 개선

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,12 +3,23 @@ import MemoList from './MemoList';
 import SideBarHeader from './SideBarHeader';
 import SideBarFooter from './SideBarFooter';
 
-function SideBar({ memos }) {
+function SideBar({
+    memos,
+    addMemo,
+    setSelectedMemoIndex,
+    selectedMemoIndex,
+    deleteMemo,
+}) {
     return (
         <div className="SideBar">
             <SideBarHeader />
-            <MemoList memos={memos} />
-            <SideBarFooter />
+            <MemoList
+                memos={memos}
+                selectedMemoIndex={selectedMemoIndex}
+                setSelectedMemoIndex={setSelectedMemoIndex}
+                deleteMemo={deleteMemo}
+            />
+            <SideBarFooter onClick={addMemo} />
         </div>
     );
 }
